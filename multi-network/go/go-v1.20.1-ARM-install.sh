14717162447fa6c1f838c976cad76a30742d3780
GLCstaked <67609618+GLCNI@users.noreply.github.com>
2023-04-20 22:34:31 +0100

    Create go-v1.20.1-ARM-install.sh

---
+++ multi-network/go/go-v1.20.1-ARM-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ver="1.20.1"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-arm64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-arm64.tar.gz"
+rm "go$ver.linux-arm64.tar.gz"
+
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
+
+# source the .bash_profile file
+source $HOME/.bash_profile
